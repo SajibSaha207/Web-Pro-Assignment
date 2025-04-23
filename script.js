2439aec8398f93b073cf85ffccf99970a6ef4c19
SajibSaha207 <sajibsaha.207@gmail.com>
2025-04-23 20:24:40 +0600

    Add HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,26 @@
+const loadBtn = document.getElementById("loadBtn");
+const dataList = document.getElementById("dataList");
+
+loadBtn.addEventListener("click", () => {
+  const saha = new XMLHttpRequest();
+  // api url: https://raw.githubusercontent.com/taher-dev/web-prog-lab-clp/refs/heads/main/get.json
+  saha.open(
+    "GET",
+    "https://raw.githubusercontent.com/taher-dev/web-prog-lab-clp/refs/heads/main/get.json",
+    true
+  );
+
+  saha.onload = () => {
+    let data = JSON.parse(saha.responseText);
+    let list = document.getElementById("dataList");
+    let str = "";
+    for (i in data) {
+      for (j in data[i]) {
+        str += `<p>${j} : ${data[i][j]}<p>`;
+      }
+    }
+    list.innerHTML = str;
+  };
+
+  saha.send();
+});
